c76fce9234ca8593d7d4a1b3afad99bc2d1f1bf3
3ANov <3anovdev@gmail.com>
2020-06-04 21:21:16 +0300

    Таки решил

--- longest substring.py
+++ longest substring.py
@@ -1,47 +1,37 @@
 #https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 class Solution(object):
-    def unique_symbols(self, s):
-        return len(set(list(s))) == len(s)
-
-
     def lengthOfLongestSubstring(self, s):
+        unique_char_set = set()
+        unique_sub_str = set()
         i = 0
-        j = 1
+        while len(s) != 0:
+            for ch in s:
+                if ch not in unique_char_set:
+                    unique_char_set.add(ch)
+                else:
+                    break
+                i += 1
+            unique_char_set.clear()
+            unique_sub_str.add(s[:i])
+            s = s[1:]
+            i = 0
+        max_length = 0
+        for k in unique_sub_str:
+            if max_length < len(k):
+                max_length = len(k)
 
-        unique_sub_set = set()
-
-        substr = s[i:j]
-        len_substr = 0
-        max_substr = ''
-        while i <= len(s):
-            while j <= len(s):
-                if self.unique_symbols(substr):
-                    unique_sub_set.add(substr)
-                j += 1
-                substr = s[i:j]
-            i += 1
-            j = i + 1
-            substr = s[i:j]
-        #print(max_substr)
-
-        for k in unique_sub_set:
-            if len_substr < len(k):
-                len_substr = len(k)
-        #print(k)
-        #print(sorted(unique_sub_set, key=len, reverse=True))
-        return len_substr
+        return max_length
 
 
 
-# x = "" #abc 3
-# print(Solution().lengthOfLongestSubstring(x))
+
+x = "dvdf"
+print(Solution().lengthOfLongestSubstring(x))
 x = "abcabcbb" #abc 3
 print(Solution().lengthOfLongestSubstring(x))
-print(Solution().unique_symbols('abcd'))
 x = "bbbbb" #b 1
 print(Solution().lengthOfLongestSubstring(x))
-print(Solution().unique_symbols(x))
 x = "pwwkew" #wke 3
 print(Solution().lengthOfLongestSubstring(x))
 x = "abcdefg" #wke 3
@@ -51,3 +41,26 @@ with open('input.txt') as file:
     data = file.read()
 
 print(Solution().lengthOfLongestSubstring(data))
+string = data
+unique_char_set = set()
+unique_sub_str = set()
+i = 0
+while len(string) != 0:
+    for ch in string:
+        if ch not in unique_char_set:
+            unique_char_set.add(ch)
+        else:
+            break
+        i += 1
+    unique_char_set.clear()
+    unique_sub_str.add(string[:i])
+    string = string[1:]
+    i = 0
+
+
+
+
+
+print(unique_char_set, i)
+print(len(sorted(unique_sub_str, key=len, reverse=True)[0]), i)
+print(string[:i])
