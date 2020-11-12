c2b6653cef8fe5ba281c8e5b7004dad373f21189
3ANov <3anovdev@gmail.com>
2020-11-12 10:38:09 +0300

    Добавил грамотное решение

---
+++ reverse_integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reverse(self, x):
+        result = 0
+        z = abs(x)
+        while z > 0:
+            result = result * 10
+            result += z % 10
+            z = z//10
+        if x < 0:
+            result = -result
+        if (result <= -2**31) or (result >= 2**31-1):
+            return 0
+        return result
+
+
+class Solution(object):
+    def reverse(self, x):
+        result = int(str(abs(x))[::-1])
+        if (result <= -2**31) or (result >= 2**31-1):
+            return 0
+        if x < 0:
+            result = -result
+        return result
+
+print(Solution().reverse(-150))
