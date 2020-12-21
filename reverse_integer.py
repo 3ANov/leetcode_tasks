f748e1f0b5cce57680fc6d240f7c6b7efcd37845
3ANov <3anovdev@gmail.com>
2020-12-21 18:27:46 +0300

    Добавил грамотное решение

--- reverse_integer.py
+++ reverse_integer.py
@@ -5,10 +5,10 @@ class Solution(object):
         while z > 0:
             result = result * 10
             result += z % 10
-            z = z//10
+            z = z // 10
         if x < 0:
             result = -result
-        if (result <= -2**31) or (result >= 2**31-1):
+        if (result <= -2 ** 31) or (result >= 2 ** 31 - 1):
             return 0
         return result
 
@@ -16,10 +16,14 @@ class Solution(object):
 class Solution(object):
     def reverse(self, x):
         result = int(str(abs(x))[::-1])
-        if (result <= -2**31) or (result >= 2**31-1):
+        if (result <= -2 ** 31) or (result >= 2 ** 31 - 1):
             return 0
         if x < 0:
             result = -result
         return result
 
+
 print(Solution().reverse(-150))
+x = [3, 0, 4, 1]
+
+print(sorted(x))
